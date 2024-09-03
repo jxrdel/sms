7452da802a59bb4f60a341f7b5ce4032295884b3
jxrdel <jrdlregis@gmail.com>
2024-09-03 11:55:12 -0400

    GitCommit2

--- resources/views/manual.blade.php
+++ resources/views/manual.blade.php
@@ -7,7 +7,7 @@
     
     <h2 style="text-align: center"><i style="color: #5D87FF" class="bi bi-question-circle-fill"></i> &nbsp; User Manual</h2>
     <br>
-    <iframe src="{{ asset('assets/manual.pdf') }}" width="100%" height="1000px"></iframe>
+    <iframe src="{{ asset('assets/smsmanual.pdf') }}" width="100%" height="1000px"></iframe>
         
           
 
